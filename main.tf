6f8a5c1fef5ca96865dda21a1e8f98e868227067
Elliot Marsden <elliot.marsden@gmail.com>
2023-06-15 17:02:11 +0100

    Add initial terraform

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "filter-govt-bills-tf"
+}
